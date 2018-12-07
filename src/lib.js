32bf9e85f7609efee1d55b01db689739b3e6eb50
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2018-12-07 17:17:33 +0530

    prettier is applied.

--- src/lib.js
+++ src/lib.js
@@ -1,18 +1,18 @@
-const illegalOptionMsg = 'head: illegal option -- ';
+const illegalOptionMsg = "head: illegal option -- ";
 
-const usageMsg = 'usage: head [-n lines | -c bytes] [file ...]';
+const usageMsg = "usage: head [-n lines | -c bytes] [file ...]";
 
-const illegalLineCountMsg = 'head: illegal line count -- ';
+const illegalLineCountMsg = "head: illegal line count -- ";
 
-const illegalByteCountMsg = 'head: illegal byte count -- ';
+const illegalByteCountMsg = "head: illegal byte count -- ";
 
-const isCount = function (string) {
+const isCount = function(string) {
   return !isNaN(string);
-}
+};
 
-const isType = function (string) {
-  return string[0] == '-' && isNaN(string[1]);
-}
+const isType = function(string) {
+  return string[0] == "-" && isNaN(string[1]);
+};
 
 /*
  * default = file1 file2;
@@ -21,79 +21,88 @@ const isType = function (string) {
  * syntax3 = -n 12 file1 file2;
  */
 
-const isSyntax1 = function (input) {
-  return input.length > 1 && isCount(input.slice(0,2));
-}
+const isSyntax1 = function(input) {
+  return input.length > 1 && isCount(input.slice(0, 2));
+};
 
-const isSyntax2 = function (input) {
+const isSyntax2 = function(input) {
   return input.length > 2 && isType(input);
-}
+};
 
-const isSyntax3 = function (input) {
+const isSyntax3 = function(input) {
   return input.length == 2 && isType(input);
-}
+};
 
-const segregateInput = function (input) {
+const segregateInput = function(input) {
   if (isSyntax1(input[0])) {
-    return {type:'n', count: input[0].slice(1), files: input.slice(1)}
+    return { type: "n", count: input[0].slice(1), files: input.slice(1) };
   }
   if (isSyntax2(input[0])) {
-    return {type: input[0][1], count: input[0].slice(2), files: input.slice(1) }
+    return {
+      type: input[0][1],
+      count: input[0].slice(2),
+      files: input.slice(1)
+    };
   }
   if (isSyntax3(input[0])) {
-    return {type: input[0][1], count: input[1], files: input.slice(2) }
+    return { type: input[0][1], count: input[1], files: input.slice(2) };
   }
-  return { type: 'n', count: '10',files: input.slice(0) };
-}
+  return { type: "n", count: "10", files: input.slice(0) };
+};
 
-const headLines = function (content,countOfLines) {
-  let lines = content.toString().split('\n');
-  lines = lines.slice(0,countOfLines).join('\n');
+const headLines = function(content, countOfLines) {
+  let lines = content.toString().split("\n");
+  lines = lines.slice(0, countOfLines).join("\n");
   return lines;
-}
+};
 
-const headCharacters = function (content,countOfChar) {
-  let characters = content.slice(0,countOfChar).toString();
+const headCharacters = function(content, countOfChar) {
+  let characters = content.slice(0, countOfChar).toString();
   return characters;
-}
+};
 
-const headFiles = function (fs,{type,count,files}) {
-  return files.map ( function (file) {
-    if ( !fs.existsSync (file) ) {
-      return 'head: '+ file +': No such file or directory';
-    }
-    let content = fs.readFileSync(file);
-    let result = headCharacters(content,count);
-    if( type == 'n') {
-      result = headLines(content,count);
-    }
-    let fileName = '==> '+ file + ' <==' + '\n';
-    if (files.length > 1) {
-      return fileName + result;
-    }
-    return result;
-  }).join("\n\n");
-}
+const headFiles = function(fs, { type, count, files }) {
+  return files
+    .map(function(file) {
+      if (!fs.existsSync(file)) {
+        return "head: " + file + ": No such file or directory";
+      }
+      let content = fs.readFileSync(file);
+      let result = headCharacters(content, count);
+      if (type == "n") {
+        result = headLines(content, count);
+      }
+      let fileName = "==> " + file + " <==" + "\n";
+      if (files.length > 1) {
+        return fileName + result;
+      }
+      return result;
+    })
+    .join("\n\n");
+};
 
-const isInvalidType = function (type) {
-  return type != 'n' && type !='c';
-}
+const isInvalidType = function(type) {
+  return type != "n" && type != "c";
+};
 
-const isInvalidCount = function (count) {
+const isInvalidCount = function(count) {
   return isNaN(count) || count < 1;
-}
+};
 
-const head = function (fs,{type,count,files}) {
+const head = function(fs, { type, count, files }) {
   if (isInvalidType(type)) {
-    return illegalOptionMsg + type + '\n' + usageMsg;
+    return illegalOptionMsg + type + "\n" + usageMsg;
   }
   if (isInvalidCount(count)) {
-    return (type == 'n') ? illegalLineCountMsg+count : illegalByteCountMsg+count;
+    return type == "n"
+      ? illegalLineCountMsg + count
+      : illegalByteCountMsg + count;
   }
-  return headFiles(fs,{type,count,files});
-}
+  return headFiles(fs, { type, count, files });
+};
 
-module.exports = { segregateInput,
+module.exports = {
+  segregateInput,
   headLines,
   headCharacters,
   headFiles,
@@ -104,4 +113,5 @@ module.exports = { segregateInput,
   isSyntax2,
   isSyntax3,
   isInvalidType,
-  isInvalidCount };
+  isInvalidCount
+};
--- test/libTest.js
+++ test/libTest.js
@@ -1,222 +1,228 @@
-const assert = require('assert');
-const { segregateInput,
-        headLines,
-        headCharacters,
-        headFiles,
-        head,
-        isType,
-        isCount,
-        isSyntax1,
-        isSyntax2,
-        isSyntax3,
-        isInvalidType,
-        isInvalidCount } = require('../src/lib.js');
+const assert = require("assert");
+const {
+  segregateInput,
+  headLines,
+  headCharacters,
+  headFiles,
+  head,
+  isType,
+  isCount,
+  isSyntax1,
+  isSyntax2,
+  isSyntax3,
+  isInvalidType,
+  isInvalidCount
+} = require("../src/lib.js");
 
 const reader = file => file;
 
 const validater = function(file) {
-  if(file == 'not exists') {
+  if (file == "not exists") {
     return false;
   }
   return true;
-}
+};
 
-const fs = { readFileSync : reader,
-             existsSync : validater }; 
+const fs = { readFileSync: reader, existsSync: validater };
 
-describe('isType', function() {
-  it('should return true if provided string is in type format', function() {
-    assert.deepEqual(isType('-n'),true);
-    assert.deepEqual(isType('-c'),true);
-    assert.deepEqual(isType('-e'),true);
+describe("isType", function() {
+  it("should return true if provided string is in type format", function() {
+    assert.deepEqual(isType("-n"), true);
+    assert.deepEqual(isType("-c"), true);
+    assert.deepEqual(isType("-e"), true);
   });
-  it('should return false if provided string is not in type format', function() {
-    assert.deepEqual(isType('fh'),false);
-  });
-});
-
-describe('isCount', function() {
-  it('should return true if provided string is number', function() {
-    assert.deepEqual(isCount('-12'),true);
-    assert.deepEqual(isCount('12'),true);
-  });
-  it('should return false if provided string is not a number', function() {
-    assert.deepEqual(isCount('45h'),false);
-    assert.deepEqual(isCount('-4hd'),false);
+  it("should return false if provided string is not in type format", function() {
+    assert.deepEqual(isType("fh"), false);
   });
 });
 
-describe('isSyntax1', function() {
+describe("isCount", function() {
+  it("should return true if provided string is number", function() {
+    assert.deepEqual(isCount("-12"), true);
+    assert.deepEqual(isCount("12"), true);
+  });
+  it("should return false if provided string is not a number", function() {
+    assert.deepEqual(isCount("45h"), false);
+    assert.deepEqual(isCount("-4hd"), false);
+  });
+});
+
+describe("isSyntax1", function() {
   it('should return true if provided input is in format "-number"', function() {
-    assert.deepEqual(isSyntax1('-12'),true);
+    assert.deepEqual(isSyntax1("-12"), true);
   });
   it('should return false if provided input is not in format "-number"', function() {
-    assert.deepEqual(isSyntax1('-n2'),false);
+    assert.deepEqual(isSyntax1("-n2"), false);
   });
 });
 
-describe('isSyntax2', function() {
+describe("isSyntax2", function() {
   it('should return true if provided input is in format "-n12"', function() {
-    assert.deepEqual(isSyntax2('-n11'),true);
-    assert.deepEqual(isSyntax2('-c4'),true);
+    assert.deepEqual(isSyntax2("-n11"), true);
+    assert.deepEqual(isSyntax2("-c4"), true);
   });
   it('should return false if provided input is not in format "-n12"', function() {
-    assert.deepEqual(isSyntax2('-12'),false);
-    assert.deepEqual(isSyntax2('-n'),false);
+    assert.deepEqual(isSyntax2("-12"), false);
+    assert.deepEqual(isSyntax2("-n"), false);
   });
 });
 
-describe('isSyntax3', function() {
+describe("isSyntax3", function() {
   it('should return true if provided input is in format "-n"', function() {
-    assert.deepEqual(isSyntax3('-n'),true);
-    assert.deepEqual(isSyntax3('-c'),true);
+    assert.deepEqual(isSyntax3("-n"), true);
+    assert.deepEqual(isSyntax3("-c"), true);
   });
   it('should return false if provided input is not in format "-n"', function() {
-    assert.deepEqual(isSyntax3('-n2'),false);
-    assert.deepEqual(isSyntax3('-12'),false);
+    assert.deepEqual(isSyntax3("-n2"), false);
+    assert.deepEqual(isSyntax3("-12"), false);
   });
 });
 
-describe('isInvalidType', function() {
-  it('should return false if provided type is one of n and c', function() {
-    assert.deepEqual(isInvalidType('n'),false);
-    assert.deepEqual(isInvalidType('c'),false);
+describe("isInvalidType", function() {
+  it("should return false if provided type is one of n and c", function() {
+    assert.deepEqual(isInvalidType("n"), false);
+    assert.deepEqual(isInvalidType("c"), false);
   });
-  it('should return true if provided type is not n nor c', function() {
-    assert.deepEqual(isInvalidType('e'),true);
-    assert.deepEqual(isInvalidType('nc'),true);
-  });
-});
-
-describe('isInvalidCount', function() {
-  it('should return false if provided count is positive integer', function() {
-    assert.deepEqual(isInvalidCount('12'),false);
-    assert.deepEqual(isInvalidCount('2'),false);
-  });
-  it('should return true if provided count is not a positive integer', function() {
-    assert.deepEqual(isInvalidCount('e12'),true);
-    assert.deepEqual(isInvalidCount('0'),true);
+  it("should return true if provided type is not n nor c", function() {
+    assert.deepEqual(isInvalidType("e"), true);
+    assert.deepEqual(isInvalidType("nc"), true);
   });
 });
 
-describe('segregateInput', function() {
-  it('should return parameter object when line/byte and count is provided combine', function() {
-    let actualInput = segregateInput(['-n5','file1']); 
-    let expectedOutput = {type:'n',count:'5',files:['file1']};
-    assert.deepEqual(actualInput,expectedOutput);
+describe("isInvalidCount", function() {
+  it("should return false if provided count is positive integer", function() {
+    assert.deepEqual(isInvalidCount("12"), false);
+    assert.deepEqual(isInvalidCount("2"), false);
   });
-
-  it('should return parameter object when only count is provide', function() {
-    let actualInput = segregateInput(['-5','file1']); 
-    let expectedOutput = {type:'n',count:'5',files:['file1']};
-    assert.deepEqual(actualInput,expectedOutput);
-  });
-
-  it('should return parameter object when line/byte and count is provided separately', function() {
-    let actualInput = segregateInput(['-c','3','file1','file2']); 
-    let expectedOutput = {type:'c',count:'3',files:['file1','file2']};
-    assert.deepEqual(actualInput,expectedOutput);
-  });
-
-  it('should return parameter object when only inputs files are provided', function() {
-    let actualInput = segregateInput(['file1','file2']); 
-    let expectedOutput = {type:'n',count:'10',files:['file1','file2']};
-    assert.deepEqual(actualInput,expectedOutput);
+  it("should return true if provided count is not a positive integer", function() {
+    assert.deepEqual(isInvalidCount("e12"), true);
+    assert.deepEqual(isInvalidCount("0"), true);
   });
 });
 
-describe('headLines', function() {
-  it('should return lines from provided input file of given count', function() {
-    let file1 = 'one\ntwo\nthree\nfour';
-    let expectedOutput = 'one\ntwo';
-    assert.deepEqual(headLines(reader(file1),2),expectedOutput);
+describe("segregateInput", function() {
+  it("should return parameter object when line/byte and count is provided combine", function() {
+    let actualInput = segregateInput(["-n5", "file1"]);
+    let expectedOutput = { type: "n", count: "5", files: ["file1"] };
+    assert.deepEqual(actualInput, expectedOutput);
+  });
+
+  it("should return parameter object when only count is provide", function() {
+    let actualInput = segregateInput(["-5", "file1"]);
+    let expectedOutput = { type: "n", count: "5", files: ["file1"] };
+    assert.deepEqual(actualInput, expectedOutput);
+  });
+
+  it("should return parameter object when line/byte and count is provided separately", function() {
+    let actualInput = segregateInput(["-c", "3", "file1", "file2"]);
+    let expectedOutput = { type: "c", count: "3", files: ["file1", "file2"] };
+    assert.deepEqual(actualInput, expectedOutput);
+  });
+
+  it("should return parameter object when only inputs files are provided", function() {
+    let actualInput = segregateInput(["file1", "file2"]);
+    let expectedOutput = { type: "n", count: "10", files: ["file1", "file2"] };
+    assert.deepEqual(actualInput, expectedOutput);
   });
 });
 
-describe('headCharacters', function() {
-  it('should return characters from provided input file of given count ', function() {
-    let file = 'one\ntwo';
-    let expectedOutput = 'one';
-    assert.deepEqual(headCharacters(reader(file),3),expectedOutput);
+describe("headLines", function() {
+  it("should return lines from provided input file of given count", function() {
+    let file1 = "one\ntwo\nthree\nfour";
+    let expectedOutput = "one\ntwo";
+    assert.deepEqual(headLines(reader(file1), 2), expectedOutput);
   });
 });
 
-describe('headFiles', function() {
-  it('should return the lines as per provided input', function() {
-    let file = 'one\ntwo\nthree\nfour';
-    let parameters = {type:'n',count:'3',files:[file]}
-    assert.deepEqual(headFiles(fs,parameters),'one\ntwo\nthree');
+describe("headCharacters", function() {
+  it("should return characters from provided input file of given count ", function() {
+    let file = "one\ntwo";
+    let expectedOutput = "one";
+    assert.deepEqual(headCharacters(reader(file), 3), expectedOutput);
+  });
+});
+
+describe("headFiles", function() {
+  it("should return the lines as per provided input", function() {
+    let file = "one\ntwo\nthree\nfour";
+    let parameters = { type: "n", count: "3", files: [file] };
+    assert.deepEqual(headFiles(fs, parameters), "one\ntwo\nthree");
   });
 
-  it('should return the characters as per provided input', function() {
-    let file = 'one\ntwo\nthree\nfour';
-    let parameters = {type:'c',count:'2',files: [file]}
-    assert.deepEqual(headFiles(fs,parameters),'on');
+  it("should return the characters as per provided input", function() {
+    let file = "one\ntwo\nthree\nfour";
+    let parameters = { type: "c", count: "2", files: [file] };
+    assert.deepEqual(headFiles(fs, parameters), "on");
   });
 
-  it('should return the lines when multiple files are provided', function() {
-    let file = 'one\ntwo\nthree';
-    let file1 = 'the\na\nan';
-    let parameters = {type:'n',count:'2',files:[file,file1]}
-    let expectedOutput = '==> one\ntwo\nthree <==\none\ntwo\n\n==> the\na\nan <==\nthe\na'
-    assert.deepEqual(headFiles(fs,parameters),expectedOutput);
+  it("should return the lines when multiple files are provided", function() {
+    let file = "one\ntwo\nthree";
+    let file1 = "the\na\nan";
+    let parameters = { type: "n", count: "2", files: [file, file1] };
+    let expectedOutput =
+      "==> one\ntwo\nthree <==\none\ntwo\n\n==> the\na\nan <==\nthe\na";
+    assert.deepEqual(headFiles(fs, parameters), expectedOutput);
   });
 
-  it('should return the characters when multiple files are provided', function() {
-    let file = 'one\ntwo';
-    let file1 = 'three\nfour';
-    let parameters = {type:'c',count:'2',files: [file,file1]}
-    let expectedOutput = '==> one\ntwo <==\non\n\n==> three\nfour <==\nth'
-    assert.deepEqual(headFiles(fs,parameters),expectedOutput);
+  it("should return the characters when multiple files are provided", function() {
+    let file = "one\ntwo";
+    let file1 = "three\nfour";
+    let parameters = { type: "c", count: "2", files: [file, file1] };
+    let expectedOutput = "==> one\ntwo <==\non\n\n==> three\nfour <==\nth";
+    assert.deepEqual(headFiles(fs, parameters), expectedOutput);
   });
 
   it("should return the lines for file which exists and error for file which doesn't exists", function() {
-    let file = 'one\ntwo\nthree';
-    let file1 = 'not exists';
-    let parameters = {type:'n',count:'2',files:[file,file1]}
-    let expectedOutput = '==> one\ntwo\nthree <==\none\ntwo\n\nhead: not exists: No such file or directory'
-    assert.deepEqual(headFiles(fs,parameters),expectedOutput);
+    let file = "one\ntwo\nthree";
+    let file1 = "not exists";
+    let parameters = { type: "n", count: "2", files: [file, file1] };
+    let expectedOutput =
+      "==> one\ntwo\nthree <==\none\ntwo\n\nhead: not exists: No such file or directory";
+    assert.deepEqual(headFiles(fs, parameters), expectedOutput);
   });
 });
 
-describe('head', function() {
-  it('should return error message when invalid type is provided ', function() {
-    let file = 'one\ntwo';
-    let file1 = 'three\nfour';
-    let parameters = {type:'e',count:'2',files: [file,file1]}
-    let expectedOutput = 'head: illegal option -- e\nusage: head [-n lines | -c bytes] [file ...]';
-    assert.deepEqual(head(fs,parameters),expectedOutput);
+describe("head", function() {
+  it("should return error message when invalid type is provided ", function() {
+    let file = "one\ntwo";
+    let file1 = "three\nfour";
+    let parameters = { type: "e", count: "2", files: [file, file1] };
+    let expectedOutput =
+      "head: illegal option -- e\nusage: head [-n lines | -c bytes] [file ...]";
+    assert.deepEqual(head(fs, parameters), expectedOutput);
   });
 
-  it('should return the lines when multiple files are provided', function() {
-    let file = 'one\ntwo\nthree';
-    let file1 = 'the\na\nan';
-    let parameters = {type:'n',count:'2',files:[file,file1]}
-    let expectedOutput = '==> one\ntwo\nthree <==\none\ntwo\n\n==> the\na\nan <==\nthe\na'
-    assert.deepEqual(head(fs,parameters),expectedOutput);
+  it("should return the lines when multiple files are provided", function() {
+    let file = "one\ntwo\nthree";
+    let file1 = "the\na\nan";
+    let parameters = { type: "n", count: "2", files: [file, file1] };
+    let expectedOutput =
+      "==> one\ntwo\nthree <==\none\ntwo\n\n==> the\na\nan <==\nthe\na";
+    assert.deepEqual(head(fs, parameters), expectedOutput);
   });
 
-  it('should return error message when provided file does not exists', function() {
-    let file = 'not exists';
-    let file1 = 'three\nfour';
-    let parameters = {type:'n',count:'2',files: [file,file1]}
-    let expectedOutput = 'head: not exists: No such file or directory\n\n==> three\nfour <==\nthree\nfour'
-    assert.deepEqual(head(fs,parameters),expectedOutput);
+  it("should return error message when provided file does not exists", function() {
+    let file = "not exists";
+    let file1 = "three\nfour";
+    let parameters = { type: "n", count: "2", files: [file, file1] };
+    let expectedOutput =
+      "head: not exists: No such file or directory\n\n==> three\nfour <==\nthree\nfour";
+    assert.deepEqual(head(fs, parameters), expectedOutput);
   });
 
-  it('should return error message when provided count is 0', function() {
-    let file = 'one\ntwo';
-    let file1 = 'three\nfour';
-    let parameters = {type:'c',count:'0',files: [file,file1]}
-    let expectedOutput = 'head: illegal byte count -- 0';
-    assert.deepEqual(head(fs,parameters),expectedOutput);
+  it("should return error message when provided count is 0", function() {
+    let file = "one\ntwo";
+    let file1 = "three\nfour";
+    let parameters = { type: "c", count: "0", files: [file, file1] };
+    let expectedOutput = "head: illegal byte count -- 0";
+    assert.deepEqual(head(fs, parameters), expectedOutput);
   });
 
-  it('should return error message when invalid count is provided', function() {
-    let file = 'one\ntwo';
-    let file1 = 'three\nfour';
-    let parameters = {type:'n',count:'10x',files: [file,file1]}
-    let expectedOutput = 'head: illegal line count -- 10x';
-    assert.deepEqual(head(fs,parameters),expectedOutput);
+  it("should return error message when invalid count is provided", function() {
+    let file = "one\ntwo";
+    let file1 = "three\nfour";
+    let parameters = { type: "n", count: "10x", files: [file, file1] };
+    let expectedOutput = "head: illegal line count -- 10x";
+    assert.deepEqual(head(fs, parameters), expectedOutput);
   });
 });
