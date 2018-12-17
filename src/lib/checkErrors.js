3eef65f91302fae6b60325b8bf718070115e3514
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2018-12-17 18:47:18 +0530

    Refactored: duplication for illegalLineCountMsg and illegalByteCountMsg is removed.

--- src/lib/checkErrors.js
+++ src/lib/checkErrors.js
@@ -2,17 +2,18 @@ const illegalOptionMsg = function (option, functionName) {
     return functionName + ": illegal option -- " + option;
 };
 
-const illegalLineCountMsg = function (count, functionName) {
-    return functionName + ": illegal line count -- " + count;
+const illegalCountMsg = function (count, option) {
+    return "head: illegal " + option + " count -- " + count;
 };
 
-const illegalByteCountMsg = function (count, functionName) {
-    return functionName + ": illegal byte count -- " + count;
+const illegalOffsetMsg = function (count) {
+    return "tail: illegal offset -- " + count;
 };
 
 const fileNotFoundMsg = function (fileName, functionName) {
     return functionName + ": " + fileName + ": No such file or directory";
 };
+
 const isInvalidOption = function (option) {
     return option != "n" && option != "c";
 };
@@ -24,25 +25,24 @@ const isInvalidCount = function (count, functionName) {
     return isNaN(count) || count < 1;
 };
 
-const illegalCountMsg = { n: illegalLineCountMsg, c: illegalByteCountMsg };
-
-const illegalOffsetMsg = "tail: illegal offset -- ";
-
 const usageMsgForHead = "usage: head [-n lines | -c bytes] [file ...]";
 
 const usageMsgForTail = "usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
 
-const usageMsg = { head: usageMsgForHead, tail: usageMsgForTail };
+const usageMsg = { head : usageMsgForHead,
+                   tail : usageMsgForTail };
+
+const invalidCountMsg = { head : illegalCountMsg,
+                           tail : illegalOffsetMsg };                           
+
+const type = { n: "line", c: "byte" };
 
 const validateInput = function ({ option, count }, functionName) {
     if (isInvalidOption(option)) {
         return illegalOptionMsg(option, functionName) + "\n" + usageMsg[functionName];
     }
     if (isInvalidCount(count, functionName)) {
-        if (functionName == 'tail') {
-            return illegalOffsetMsg + count;
-        }
-        return illegalCountMsg[option](count, functionName);
+        return invalidCountMsg[functionName](count, type[option]);
     }
 };
 
