dfcb76ccfa4c74b55854d3251dae13a9dc238f18
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2018-12-19 12:03:54 +0530

    Refactored.
    parse is again changed to previous name segregateInput.
    parse is introduced as new function which will convert count and option.

--- src/io/parseInput.js
+++ src/io/parseInput.js
@@ -18,26 +18,42 @@ const isOnlyOption = function (input) {
   return input.length == 2 && isOption(input);
 };
 
+const isValidOption = function (option) {
+  return option == 'n' || option == 'c';
+};
+
 const options = { n : "line", c : "byte" };
 
-const parse = function (input) {
+const segregateInput = function (input) {
   if (isOnlyCount(input[0])) {
-    return { option: "line", count: +input[0].slice(1), files: input.slice(1) };
+    return { option: "n", count: input[0].slice(1), files: input.slice(1) };
   };
   if (isOptionAndCount(input[0])) {
     return {
-      option: options[input[0][1]],
-      count: +input[0].slice(2),
+      option: input[0][1],
+      count: input[0].slice(2),
       files: input.slice(1)
     };
   };
   if (isOnlyOption(input[0])) {
-    return { option: options[input[0][1]], count: +input[1], files: input.slice(2) };
+    return { option: input[0][1], count: input[1], files: input.slice(2) };
   };
-  return { option: "line", count: 10, files: input.slice(0) };
+  return { option: "n", count: 10, files: input.slice(0) };
+};
+
+const parse = function (input) {
+  let parameters = segregateInput(input);
+  if (isValidOption(parameters.option)) {
+    parameters.option = options[parameters.option];
+  };
+  if (!isNaN(parameters.count)) {
+    parameters.count = +parameters.count;
+  };
+  return parameters;
 };
 
 module.exports = {
+  segregateInput,
   parse,
   isOnlyCount,
   isOnlyOption,
